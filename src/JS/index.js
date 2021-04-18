69d8cf8916fcbc95167fcb6ef743d44e7c7d71aa
Leonardo Rincón <jleobotache@gmail.com>
2021-04-18 10:57:52 -0500

    adding icons delete edit and check button, remove the default lock from submit that prevented the modal from closing and adding styles to the icons

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,19 +1,18 @@
 import { Task } from "./task.js";
-import { UI } from "./DOM.js";
+import { DOM } from "./DOM.js";
 
 //***** firebase configuration code ***************** */
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBz535g3lAKXWFX_Yj_Y5bbkQtjzuqcdn4",
-    authDomain: "todo-platzimaster.firebaseapp.com",
-    projectId: "todo-platzimaster",
-    storageBucket: "todo-platzimaster.appspot.com",
-    messagingSenderId: "229804248567",
-    appId: "1:229804248567:web:9d033d0f00398511d472f2"
+  apiKey: "AIzaSyBz535g3lAKXWFX_Yj_Y5bbkQtjzuqcdn4",
+  authDomain: "todo-platzimaster.firebaseapp.com",
+  projectId: "todo-platzimaster",
+  storageBucket: "todo-platzimaster.appspot.com",
+  messagingSenderId: "229804248567",
+  appId: "1:229804248567:web:9d033d0f00398511d472f2",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 
 //***** code of modal task ***************** */
 
@@ -23,33 +22,33 @@ const modal = document.getElementById("modal");
 const ModalContainer = document.querySelector(".modal-cantainer");
 
 const openModal = () => {
-    ModalContainer.classList.toggle("modal-visivility");
-    modal.classList.toggle("modal-close");
+  ModalContainer.classList.toggle("modal-visivility");
+  modal.classList.toggle("modal-close");
 };
 
 const close_Modal = () => {
-    let timeModalGo = 600;
-    modal.classList.toggle("modal-close");
-    setTimeout(() => {
-        ModalContainer.classList.toggle("modal-visivility");
-    }, timeModalGo);
-    const ui = new UI();
-    ui.resetForm();
+  let timeModalGo = 600;
+  modal.classList.toggle("modal-close");
+  setTimeout(() => {
+    ModalContainer.classList.toggle("modal-visivility");
+  }, timeModalGo);
+  const dom = new DOM();
+  dom.resetForm();
 };
 
 addTask.addEventListener("click", openModal);
 closeModal.addEventListener("click", close_Modal);
 
 window.addEventListener("click", (e) => {
-    if (e.target == ModalContainer) {
-        let timeModalGo = 600;
-        modal.classList.toggle("modal-close");
-        setTimeout(() => {
-            ModalContainer.classList.toggle("modal-visivility");
-        }, timeModalGo);
-        const ui = new UI();
-        ui.resetForm();
-    }
+  if (e.target == ModalContainer) {
+    let timeModalGo = 600;
+    modal.classList.toggle("modal-close");
+    setTimeout(() => {
+      ModalContainer.classList.toggle("modal-visivility");
+    }, timeModalGo);
+    const dom = new DOM();
+    dom.resetForm();
+  }
 });
 
 // ************************** Modal Statistics
@@ -60,78 +59,77 @@ const statisticsModal = document.querySelector(".modal-statistics");
 const btnCloseStatistic = document.getElementById("close_modal_statistics");
 
 const openStatistics = () => {
-    modalStatistic.classList.toggle("modal-visivility__statistics");
-    statisticsModal.classList.toggle("modal-statistics__close");
+  modalStatistic.classList.toggle("modal-visivility__statistics");
+  statisticsModal.classList.toggle("modal-statistics__close");
 };
 
 const closeStatistics = () => {
-    let timeModalGo = 600;
-    statisticsModal.classList.toggle("modal-statistics__close");
-    setTimeout(() => {
-        modalStatistic.classList.toggle("modal-visivility__statistics");
-    }, timeModalGo);
+  let timeModalGo = 600;
+  statisticsModal.classList.toggle("modal-statistics__close");
+  setTimeout(() => {
+    modalStatistic.classList.toggle("modal-visivility__statistics");
+  }, timeModalGo);
 };
 
 statistic.addEventListener("click", openStatistics);
 btnCloseStatistic.addEventListener("click", closeStatistics);
 
 window.addEventListener("click", (e) => {
-    if (e.target == modalStatistic) {
-        let timeModalGo = 600;
-        statisticsModal.classList.toggle("modal-statistics__close");
-        setTimeout(() => {
-            modalStatistic.classList.toggle("modal-visivility__statistics");
-        }, timeModalGo);
-    }
+  if (e.target == modalStatistic) {
+    let timeModalGo = 600;
+    statisticsModal.classList.toggle("modal-statistics__close");
+    setTimeout(() => {
+      modalStatistic.classList.toggle("modal-visivility__statistics");
+    }, timeModalGo);
+  }
 });
 
 //************************** Code of Tasks */
 
 // DOM Events
-document.getElementById("task-form").addEventListener("submit", function(e) {
-    // Override the default Form behaviour
-    e.preventDefault();
+document.getElementById("task-form").addEventListener("submit", function (e) {
+  // Override the default Form behaviour
+  // e.preventDefault();
 
-    // Getting Form Values
-    const name = document.getElementById("name").value,
-        status = document.getElementById("status").value,
-        time = document.getElementById("time").value,
-        description = document.getElementById("description").value;
+  // Getting Form Values
+  const name = document.getElementById("name").value,
+    status = document.getElementById("status").value,
+    time = document.getElementById("time").value,
+    description = document.getElementById("description").value;
 
+  // Create a new DOM instance
+  const dom = new DOM();
 
+  // Input User Validation
+  if (name === "" || status === "" || time === "" || description === "") {
+    return dom.showMessage("Please Insert data in all fields", "danger");
+  }
 
-    // Create a new UI instance
-    const ui = new UI();
+  // Save Task
 
-    // Input User Validation
-    if (name === "" || status === "" || time === "" || description === "") {
-        return ui.showMessage("Please Insert data in all fields", "danger");
-    }
+  let key = firebase.database().ref().child("unfinished_task").push().key;
+  let updates = {};
+  // Create a new Oject Task
+  const task = new Task(key, name, status, time, description);
+  updates["/unfinished_task/" + key] = task;
+  firebase.database().ref().update(updates);
 
-    // Save Task
-
-    let key = firebase.database().ref().child("unfinished_task").push().key;
-    let updates = {};
-    // Create a new Oject Task
-    const task = new Task(key, name, status, time, description);
-    updates["/unfinished_task/" + key] = task;
-    firebase.database().ref().update(updates);
-
-    ui.addTask(task);
-    ui.showMessage("task Added Successfully", "success");
-    ui.resetForm();
+  dom.addTask(task);
+  dom.showMessage("task Added Successfully", "success");
+  dom.resetForm();
 });
 
 document.getElementById("task-list").addEventListener("click", (e) => {
-    const ui = new UI();
-    ui.deleteTask(e.target);
-    const key = e.target.parentElement.parentElement.getAttribute("data-key");
-    var task_to_remove = firebase.database().ref("unfinished_task/" + key);
-    task_to_remove.remove();
+  const dom = new DOM();
+  dom.deleteTask(e.target);
+  // console.log(e);
 
-    e.preventDefault();
+  const key = e.target.parentElement.parentElement.getAttribute("data-key");
+  var task_to_remove = firebase.database().ref("unfinished_task/" + key);
+  task_to_remove.remove();
+
+  // e.preventDefault();
 });
-
 
 //*************************** Drag and Drop  */
 
@@ -140,46 +138,55 @@ const listTask = document.getElementById("task-list");
 // console.log(listTask);
 
 Sortable.create(listTask, {
-    animation: 150,
-    chosenClass: "taskSelect",
-    ghostClass: "fantasma",
-    dragClass: "drag",
+  animation: 150,
+  chosenClass: "taskSelect",
+  ghostClass: "fantasma",
+  dragClass: "drag",
 
-    // onEnd: () => {
-    //   console.log("se cambio el orden");
-    // },
+  // onEnd: () => {
+  //   console.log("se cambio el orden");
+  // },
 
-    group: "listask",
-    store: {
-        set: (sortable) => {
-            const orden = sortable.toArray();
-            // console.log(orden);
-            localStorage.setItem(sortable.options.group.name, orden.join("|"));
-        },
-
-        get: (sortable) => {
-            const orden = localStorage.getItem(sortable.options.group.name);
-            return orden ? orden.split("|") : [];
-        },
+  group: "listask",
+  store: {
+    set: (sortable) => {
+      const orden = sortable.toArray();
+      // console.log(orden);
+      localStorage.setItem(sortable.options.group.name, orden.join("|"));
     },
+
+    get: (sortable) => {
+      const orden = localStorage.getItem(sortable.options.group.name);
+      return orden ? orden.split("|") : [];
+    },
+  },
 });
 
 function load_tasks() {
-    // task_container = document.getElementById("task-list")[0];
-    // task_container.innerHTML = "";
-    const ui = new UI();
+  // task_container = document.getElementById("task-list")[0];
+  // task_container.innerHTML = "";
+  const dom = new DOM();
 
-    const task_array = [];
-    firebase.database().ref("unfinished_task").once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            let childKey = childSnapshot.key;
-            let childData = childSnapshot.val();
-            task_array.push(Object.values(childData));
-        });
-        for (let i = 0; i < task_array.length; i++) {
-            let task = new Task(task_array[i][1], task_array[i][2], task_array[i][3], task_array[i][4], task_array[i][0]);
-            ui.addTask(task);
-        }
+  const task_array = [];
+  firebase
+    .database()
+    .ref("unfinished_task")
+    .once("value", function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        let childKey = childSnapshot.key;
+        let childData = childSnapshot.val();
+        task_array.push(Object.values(childData));
+      });
+      for (let i = 0; i < task_array.length; i++) {
+        let task = new Task(
+          task_array[i][1],
+          task_array[i][2],
+          task_array[i][3],
+          task_array[i][4],
+          task_array[i][0]
+        );
+        dom.addTask(task);
+      }
     });
 }
 
